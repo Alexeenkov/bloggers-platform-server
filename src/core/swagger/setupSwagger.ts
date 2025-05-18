93de9dab6ba90c179b915785d4cb55243ee3a8fb
Ilya Alekseenkov <alekseenkov.i@promail366.com>
2025-05-18 20:52:20 +0300

    add new swagger method

--- src/core/swagger/setupSwagger.ts
+++ src/core/swagger/setupSwagger.ts
@@ -1,23 +1,35 @@
-import swaggerJsdoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
-import { Express } from 'express';
+// import swaggerJsdoc from 'swagger-jsdoc';
+// import swaggerUi from 'swagger-ui-express';
+import {Express, Request, Response} from 'express';
+import path from "path";
+import {HTTP_STATUSES} from "../../shared/constants/httpStatuses";
 
-const swaggerOptions = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Bloggers platform',
-      version: '1.0.0',
-      description: 'Bloggers platform API. Всё для Евгения',
-    },
-  },
-  apis: ['./**/*.swagger.yml'],
-};
-
-const swaggerSpec = swaggerJsdoc(swaggerOptions);
-
-console.log(swaggerSpec);
+// const swaggerOptions = {
+//   definition: {
+//     openapi: '3.0.0',
+//     info: {
+//       title: 'Bloggers platform',
+//       version: '1.0.0',
+//       description: 'Bloggers platform API. Всё для Евгения',
+//     },
+//   },
+//   apis: ['./**/*.swagger.yml'],
+// };
+//
+// const swaggerSpec = swaggerJsdoc(swaggerOptions);
+//
+// console.log(swaggerSpec);
 
 export const setupSwagger = (app: Express) => {
-  app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.get('/api', (_: Request, res: Response) => {
+    const filePath = path.resolve(__dirname, 'swagger.html');
+
+    res.sendFile(filePath, (err) => {
+      if (err) {
+        res.status(HTTP_STATUSES.NOT_FOUND).send(`File not found by path: ${filePath}`);
+      }
+    });
+  });
+
+  // app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 };
