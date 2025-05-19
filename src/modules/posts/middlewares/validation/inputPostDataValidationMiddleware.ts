4b06996786b3a667cb89d4844a247b4d0a3462fa
Ilya Alekseenkov <alekseenkov.i@promail366.com>
2025-05-19 10:17:26 +0300

    fix

--- src/modules/posts/middlewares/validation/inputPostDataValidationMiddleware.ts
+++ src/modules/posts/middlewares/validation/inputPostDataValidationMiddleware.ts
@@ -10,13 +10,13 @@ const shortDescriptionValidation =
     body('shortDescription')
         .isString().withMessage('shortDescription should be string')
         .trim()
-        .isLength({ min: 10, max: 100 }).withMessage('Length of shortDescription is not correct (must be from 10 to 100 characters)');
+        .isLength({ min: 2, max: 100 }).withMessage('Length of shortDescription is not correct (must be from 2 to 100 characters)');
 
 const contentValidation =
     body('content')
         .isString().withMessage('content should be string')
         .trim()
-        .isLength({ max: 1000 }).withMessage('Length of content is not correct (must be from 10 to 1000 characters)');
+        .isLength({ min: 2, max: 1000 }).withMessage('Length of content is not correct (must be from 2 to 1000 characters)');
 
 const blogIdValidation =
     body('blogId')
