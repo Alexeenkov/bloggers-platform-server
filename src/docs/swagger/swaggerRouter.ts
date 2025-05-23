f8a0202ae93f23e63635f22671566f45c144f56c
Ilya Alekseenkov <alekseenkov.i@promail366.com>
2025-05-23 14:16:13 +0300

    fix

--- src/docs/swagger/swaggerRouter.ts
+++ src/docs/swagger/swaggerRouter.ts
@@ -2,21 +2,18 @@ import {Request, Response, Router} from 'express';
 import path from "path";
 import {HTTP_STATUSES} from "../../shared/constants/httpStatuses";
 
-const getSwaggerRouterHandler = (_: Request, res: Response) => {
-  const filePath = path.resolve(__dirname, 'index.html');
-
-  res.sendFile(filePath, (err) => {
-    if (err) {
-      res.status(HTTP_STATUSES.NOT_FOUND).send(`File not found by path: ${filePath}`);
-    }
-  });
-};
-
 export const getSwaggerRouter = () => {
   const router = Router({});
 
-  router.get('', getSwaggerRouterHandler);
-  router.get('/', getSwaggerRouterHandler);
+  router.get('/', (_: Request, res: Response) => {
+    const filePath = path.resolve(__dirname, 'index.html');
+
+    res.sendFile(filePath, (err) => {
+      if (err) {
+        res.status(HTTP_STATUSES.NOT_FOUND).send(`File not found by path: ${filePath}`);
+      }
+    });
+  });
 
   router.get('/specSwagger.js', (_: Request, res: Response) => {
     const filePath = path.resolve(__dirname, 'specSwagger.js');
