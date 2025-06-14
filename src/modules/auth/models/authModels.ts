ade3f2209fe1bd57473c614e54eb464cdc796e4e
Ilya Alekseenkov <alekseenkov.i@promail366.com>
2025-06-14 17:28:17 +0300

    add auth module

---
+++ src/modules/auth/models/authModels.ts
@@ -0,0 +1,1 @@
+export interface LoginInputDataModel {    loginOrEmail: string    password: string}
--- src/shared/constants/routersPaths.ts
+++ src/shared/constants/routersPaths.ts
@@ -2,3 +2,5 @@ export const API_ROUTER_PATH = '/api';
 export const BLOGS_ROUTER_PATH = `${API_ROUTER_PATH}/blogs`;
 export const POSTS_ROUTER_PATH = `${API_ROUTER_PATH}/posts`;
 export const USERS_ROUTER_PATH = `${API_ROUTER_PATH}/users`;
+
+export const AUTH_ROUTER_PATH = `${API_ROUTER_PATH}/auth`
