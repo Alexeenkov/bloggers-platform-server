05741593544c18b0d4ca2fffece12c9d51f4eceb
Ilya Alekseenkov <alekseenkov.i@promail366.com>
2025-11-04 15:11:39 +0300

    fix

--- src/shared/appConfig.ts
+++ src/shared/appConfig.ts
@@ -3,7 +3,7 @@ import {config} from "dotenv";
 config();
 
 export const appConfig = {
-    port: process.env.PORT,
+    port: process.env.PORT || 3003,
     mongoUri: process.env.MONGO_URL,
     adminUsername: process.env.ADMIN_USERNAME,
     adminPassword: process.env.ADMIN_PASSWORD,
